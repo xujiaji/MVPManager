9faf9bbd32831941adfaba27673f89d79a2fe193
e <jiajixu@qq.com>
2017-06-16 16:31:23 +0800

    Add the AutoComplete feature

--- src/io/xujiaji/plugin/dialog/EditorMVPDialog.java
+++ src/io/xujiaji/plugin/dialog/EditorMVPDialog.java
@@ -1,7 +1,9 @@
 package io.xujiaji.plugin.dialog;
 
 import com.intellij.ide.util.PropertiesComponent;
+import com.intellij.openapi.ui.ComboBox;
 import com.intellij.openapi.ui.Messages;
+import com.intellij.psi.PsiFile;
 import com.intellij.ui.JBColor;
 import io.xujiaji.plugin.Constant;
 import io.xujiaji.plugin.listener.EditorListener;
@@ -14,10 +16,7 @@ import io.xujiaji.plugin.widget.InputMethodDialog;
 import org.apache.http.util.TextUtils;
 
 import javax.swing.*;
-import javax.swing.event.ChangeEvent;
-import javax.swing.event.ChangeListener;
-import javax.swing.event.TableModelEvent;
-import javax.swing.event.TableModelListener;
+import javax.swing.event.*;
 import javax.swing.table.DefaultTableModel;
 import javax.swing.table.TableModel;
 import java.awt.*;
@@ -25,6 +24,9 @@ import java.awt.event.*;
 import java.io.IOException;
 import java.net.URISyntaxException;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Locale;
+import java.util.Map;
 
 public class EditorMVPDialog extends JDialog {
     private JPanel contentPane;
@@ -87,13 +89,38 @@ public class EditorMVPDialog extends JDialog {
     private void initView() {
         labelUrl.setForeground(JBColor.BLUE);
         xmvpCheckBox.setSelected(PropertiesComponent.getInstance().getBoolean(Constant.IS_XMVP));
+        viewImpName.setColumns(30);
         if (initEntity == null) return;
         String[] datasPackage = new String[initEntity.getPsiDirectories().length];
+        Map<Object, String[]> maps = new HashMap<>();
         for (int i = 0; i < datasPackage.length; i++) {
             datasPackage[i] = initEntity.getPsiDirectories()[i].getName();
+            PsiFile[] psiFiles = initEntity.getPsiDirectories()[i].getFiles();
+            String[] strs = new String[psiFiles.length];
+            for (int j = 0; j < psiFiles.length; j++) {
+                strs[j] = psiFiles[j].getName().replace(".java", "");
+            }
+            maps.put(initEntity.getPsiDirectories()[i].getName(), strs);
         }
         ComboBoxModel<String> comboBoxModel = new DefaultComboBoxModel<>(datasPackage);
         viewPackageName.setModel(comboBoxModel);
+        viewPackageName.addItemListener(new ItemListener() {
+            @Override
+            public void itemStateChanged(ItemEvent e) {
+                setupAutoComplete(viewImpName, maps.get(e.getItem()));
+            }
+        });
+    }
+
+    private static boolean isAdjusting(JComboBox cbInput) {
+        if (cbInput.getClientProperty("is_adjusting") instanceof Boolean) {
+            return (Boolean) cbInput.getClientProperty("is_adjusting");
+        }
+        return false;
+    }
+
+    private static void setAdjusting(JComboBox cbInput, boolean adjusting) {
+        cbInput.putClientProperty("is_adjusting", adjusting);
     }
 
     private void addListener() {
@@ -347,5 +374,84 @@ public class EditorMVPDialog extends JDialog {
             }
         };
         return mJtable;
+    }
+
+    public static void setupAutoComplete(final JTextField txtInput, final String[] items) {
+        final DefaultComboBoxModel model = new DefaultComboBoxModel();
+        final JComboBox cbInput = new JComboBox(model) {
+            public Dimension getPreferredSize() {
+                return new Dimension(super.getPreferredSize().width, 0);
+            }
+        };
+        setAdjusting(cbInput, false);
+        for (String item : items) {
+            model.addElement(item);
+        }
+        cbInput.setSelectedItem(null);
+        cbInput.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if (!isAdjusting(cbInput)) {
+                    if (cbInput.getSelectedItem() != null) {
+                        txtInput.setText(cbInput.getSelectedItem().toString());
+                    }
+                }
+            }
+        });
+
+        txtInput.addKeyListener(new KeyAdapter() {
+
+            @Override
+            public void keyPressed(KeyEvent e) {
+                setAdjusting(cbInput, true);
+                if (e.getKeyCode() == KeyEvent.VK_SPACE) {
+                    if (cbInput.isPopupVisible()) {
+                        e.setKeyCode(KeyEvent.VK_ENTER);
+                    }
+                }
+                if (e.getKeyCode() == KeyEvent.VK_ENTER || e.getKeyCode() == KeyEvent.VK_UP || e.getKeyCode() == KeyEvent.VK_DOWN) {
+                    e.setSource(cbInput);
+                    cbInput.dispatchEvent(e);
+                    if (e.getKeyCode() == KeyEvent.VK_ENTER) {
+                        txtInput.setText(cbInput.getSelectedItem().toString());
+                        cbInput.setPopupVisible(false);
+                    }
+                }
+                if (e.getKeyCode() == KeyEvent.VK_ESCAPE) {
+                    cbInput.setPopupVisible(false);
+                }
+                setAdjusting(cbInput, false);
+            }
+        });
+        txtInput.getDocument().addDocumentListener(new DocumentListener() {
+            public void insertUpdate(DocumentEvent e) {
+                updateList();
+            }
+
+            public void removeUpdate(DocumentEvent e) {
+                updateList();
+            }
+
+            public void changedUpdate(DocumentEvent e) {
+                updateList();
+            }
+
+            private void updateList() {
+                setAdjusting(cbInput, true);
+                model.removeAllElements();
+                String input = txtInput.getText();
+                if (!input.isEmpty()) {
+                    for (String item : items) {
+                        if (item.toLowerCase().startsWith(input.toLowerCase())) {
+                            model.addElement(item);
+                        }
+                    }
+                }
+                cbInput.setPopupVisible(model.getSize() > 0);
+                setAdjusting(cbInput, false);
+            }
+        });
+        txtInput.setLayout(new BorderLayout());
+        txtInput.add(cbInput, BorderLayout.SOUTH);
     }
 }
