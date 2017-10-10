080923afa6227ef341d4c5baeabcdb155dd7301c
xuoocc <xuoocc@gmail.com>
2017-10-10 11:05:38 +0800

    新增可以将所有文件放一个包中       通过点选按钮切换，文件创建位置       增加预览创建文件位置

---
+++ src/io/xujiaji/plugin/util/UIUtil.java
@@ -0,0 +1,34 @@
+package io.xujiaji.plugin.util;
+
+import javax.swing.*;
+import javax.swing.tree.TreeNode;
+import javax.swing.tree.TreePath;
+import java.util.Enumeration;
+
+public class UIUtil
+{
+    public static void expandTree(JTree tree) {
+        TreeNode root = (TreeNode) tree.getModel().getRoot();
+        expandAll(tree, new TreePath(root), true);
+    }
+
+
+    private static void expandAll(JTree tree, TreePath parent, boolean expand) {
+        // Traverse children
+        TreeNode node = (TreeNode) parent.getLastPathComponent();
+        if (node.getChildCount() >= 0) {
+            for (Enumeration e = node.children(); e.hasMoreElements(); ) {
+                TreeNode n = (TreeNode) e.nextElement();
+                TreePath path = parent.pathByAddingChild(n);
+                expandAll(tree, path, expand);
+            }
+        }
+
+        // Expansion or collapse must be done bottom-up
+        if (expand) {
+            tree.expandPath(parent);
+        } else {
+            tree.collapsePath(parent);
+        }
+    }
+}
